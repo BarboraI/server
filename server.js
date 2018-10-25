e565aa177ec7a9323e1612f587d3713cd8b24669
unknown <barb.ivankova@gmail.com>
2018-10-25 09:17:47 +0200

    capa xml

--- server.js
+++ server.js
@@ -3,3 +3,29 @@ var leco = 6
 var sum = polievka + leco
 console.log (sum  + "Vsetko je ok");
 
+var express = require("express");
+var path = require("path");
+var server = express();
+var PORT = 3000
+
+
+server.get('/getCapabilities', function (request, response) {
+   response.sendFile(path.join(__dirname + "/getCappabilitiesDocuments.xml"))
+
+})
+
+server.get ("/Ahoj/neviem", function (request, response) {
+    console.log(request.query.stvrtok);
+  response.send(request.query);
+});
+
+
+server.get("/some/path", function(request, response) {
+    console.log(request.query.stvrtok);
+    response.send(request.query);
+  });
+
+
+server.listen(PORT, function() {
+    console.log("Server listening on port " + PORT + "!");
+  });
